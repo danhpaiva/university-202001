e1f3bb1a1c07b28b3b1856b47794a88d4746b682
Paiva <danpaiva@live.com>
2021-01-21 20:14:47 -0300

    Slide 02 e Exercício 01

---
+++ e01-avulso/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace e01_avulso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\tLeitor de Endereços");
+
+            Console.WriteLine("\tInforme sua rua: ");
+            string rua = Console.ReadLine();
+
+            Console.WriteLine("\tInforme o número da sua casa: ");
+            string numero = Console.ReadLine();
+
+            Console.WriteLine("\tInforme seu bairro: ");
+            string bairro = Console.ReadLine();
+
+            Console.WriteLine("\tInforme sua cidade: ");
+            string cidade = Console.ReadLine();
+
+            Console.WriteLine("\tInforme seu estado: ");
+            string estado = Console.ReadLine();
+
+            Console.WriteLine("\tInforme seu país: ");
+            string pais = Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine($"\n\tSeu endereço é:\nRua: {rua} Número: {numero}\nBairro: {bairro}\nCidade:{cidade}\nEstado: {estado}\nPaís: {pais}");
+        }
+    }
+}
---
+++ slide2-e2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace slide2_e2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nCálculo da Área do Triângulo");
+
+            double b = 0, a = 0, r = 0;
+
+            Console.WriteLine("\tInforme a base do triângulo: ");
+            b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\tInforme a altura do triângulo: ");
+            a = double.Parse(Console.ReadLine());
+
+            //Cálculo
+            r = (b * a) / 2;
+
+            Console.WriteLine($"\tO resultado da área é: {r} !");
+        }
+    }
+}
