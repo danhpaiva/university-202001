948d49e81ca0b325bdaa7260faa24537a06fa146
Paiva <danpaiva@live.com>
2021-06-26 22:01:53 -0300

    update variables

--- slide2-e1/Program.cs
+++ slide2-e1/Program.cs
@@ -8,15 +8,15 @@ namespace slide2_e1
         {
             Console.WriteLine("\nCalculating the Math Power of a Number");
 
-            double base_power = 0, exponent = 0, result = 0;
+            double basePower = 0, exponent = 0, result = 0;
 
             Console.WriteLine("Inform the power base: ");
-            base_power = int.Parse(Console.ReadLine()); //TypeCast
+            basePower = int.Parse(Console.ReadLine()); //TypeCast
 
             Console.WriteLine("Inform the power exponent: ");
             exponent = int.Parse(Console.ReadLine()); //TypeCast
 
-            result = Math.Pow(base_power, exponent);
+            result = Math.Pow(basePower, exponent);
 
             Console.WriteLine($"\nThe result of the potency is: {result} .");
         }
